45f8772788bf8fd171c55e6697e149256720e724
Alfonsina <alfonsina.l@gmail.com>
2020-10-24 20:02:54 -0300

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-import React, { Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
+import Clima from './components/Clima';
 import Header from './components/Header';
+import Error from './components/Error';
+import API_KEY from './API_KEY'
 
 function App() {
+  // state del formulario
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  })
+  const [consultar, guardarConsultar] = useState(false)
+  const [resultado, guardarResultado] = useState({})
+  const [error, guardarError] = useState(false)
+
+  const { ciudad, pais } = busqueda
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if (consultar) {
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${API_KEY}`
+
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+
+        guardarResultado(resultado)
+        guardarConsultar(false)
+
+        //Detecta si hubo resultados correctos en la consulta
+        if (resultado.cod === "404") {
+          guardarError(true)
+        } else {
+          guardarError(false)
+        }
+      }
+    }
+    consultarAPI()
+    //eslint-disable-next-line
+  }, [consultar])
+
+  let componente
+  if (error) {
+    componente = <Error mensaje="No hay resultados" />
+  } else {
+    componente = <Clima resultado={resultado} />
+  }
+
   return (
     <>
       <Header titulo='Clima React App' />
@@ -10,10 +54,14 @@ function App() {
         <div className="container">
           <div className="row">
             <div className="col m6 s12">
-              <Formulario />
+              <Formulario
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsultar={guardarConsultar}
+              />
             </div>
             <div className="col m6 s12">
-              2
+              {componente}
             </div>
           </div>
         </div>
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Error from './Error'
+import PropTypes from 'prop-types'
 
-const Formulario = () => {
+const Formulario = ({ busqueda, guardarBusqueda, guardarConsultar }) => {
+  const [error, guardarError] = useState(false)
+
+  //extraer ciudad y pais
+  const { ciudad, pais } = busqueda
+
+  //Función que coloca los elementos en el state
+  const handleChange = e => {
+    //actualizar el state
+    guardarBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  // Al hacer submit al form
+  const handleSubmit = e => {
+    e.preventDefault()
+    //validar
+    if (ciudad.trim() === '' || pais.trim() === '') {
+      guardarError(true)
+      return;
+    }
+    guardarError(false)
+    // pasar al componente principal
+    guardarConsultar(true)
+  }
+
   return (
-    <form>
+    <form
+      onSubmit={(e) => handleSubmit(e)}
+    >
+      {error && <Error mensaje="Ambos campos son obligatorios" />}
       <div className="input-field col s12">
         <input
           type="text"
           name="ciudad"
           id="ciudad"
+          value={ciudad}
+          onChange={handleChange}
         />
         <label htmlFor="ciudad">Ciudad: </label>
       </div>
@@ -15,13 +49,32 @@ const Formulario = () => {
         <select
           name="pais"
           id="pais"
+          value={pais}
+          onChange={handleChange}
         >
           <option value="">-- Seleccione un país --</option>
+          <option value="US">Estados Unidos</option>
+          <option value="MX">México</option>
+          <option value="AR">Argentina</option>
+          <option value="CO">Colombia</option>
+          <option value="CR">Costa Rica</option>
+          <option value="ES">España</option>
+          <option value="PE">Perú</option>
+          <option value="CL">Chile</option>
         </select>
         <label htmlFor="pais">País</label>
       </div>
+      <div className="input-field col s12">
+        <button type="submit" className=" black-text waves-effect waves-light btn-large btn-block yellow accent-4">Buscar Clima</button>
+      </div>
     </form>
   )
+}
+
+Formulario.propTypes = {
+  busqueda: PropTypes.object.isRequired,
+  guardarBusqueda: PropTypes.func.isRequired,
+  guardarConsultar: PropTypes.func.isRequired
 }
 
 export default Formulario
